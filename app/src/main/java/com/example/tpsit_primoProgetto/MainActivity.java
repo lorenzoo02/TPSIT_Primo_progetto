f9e29506e8f316c591a54ae1a26bd5e473e4a017
Lorenzo Popescu <lorenzopopescu79@gmail.com>
2021-10-21 08:16:54 +0200

    a

--- app/src/main/java/com/example/tpsit_primoProgetto/MainActivity.java
+++ app/src/main/java/com/example/tpsit_primoProgetto/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.tpsit_primoProgetto;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button reset, test;
+    TextView testo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reset = findViewById(R.id.reset);
+        test = findViewById(R.id.test);
+        testo = findViewById(R.id.testo);
     }
+
+    public void reset(View v){
+        testo.setText("");
+    }
+
+    public void test(View v){
+        testo.setText("Messaggio di prova");
+    }
+
+
 }
